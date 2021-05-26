bec02c3ef238a448ba3ede6b81fb543db55f9cf7
ldiamantopoulos <ldiamantopoulos@sqlearn.com>
2021-05-26 15:00:27 +0300

    Initial commit

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(QuestionRepository $repository)
+    {
+        /*
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+        */
+
+        $questions = $repository->findAllAskedOrderedByNewest();
+
+
+        return $this->render('question/homepage.html.twig', ['questions' => $questions]);
+    }
+
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-' . rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if(rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $question->setVotes(rand(-20, 50));
+
+        $entityManager->persist($question);
+        $entityManager->flush();
+
+        return new Response(sprintf('Well hallo! The shiny new question is id #%d, slug %s',
+        $question->getId(),
+        $question->getSlug(),
+        ));
+    }
+
+
+    /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     */
+    public function show(Question $question)
+    {
+
+
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'answers' => $answers,
+        ]);
+    }
+
+    /**
+     * @Route ("/questions/{slug}/vote", name="app_question_vote", methods="POST" )
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager){
+        $direction = $request->request->get('direction');
+        if ($direction === 'up') {
+            $question->upVote();
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug(),
+
+        ]);
+    }
+
+
+}
---
+++ src/Factory/QuestionFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
+use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @method static Question|Proxy findOrCreate(array $attributes)
+ * @method static Question|Proxy random()
+ * @method static Question[]|Proxy[] randomSet(int $number)
+ * @method static Question[]|Proxy[] randomRange(int $min, int $max)
+ * @method static QuestionRepository|RepositoryProxy repository()
+ * @method Question|Proxy create($attributes = [])
+ * @method Question[]|Proxy[] createMany(int $number, $attributes = [])
+ */
+final class QuestionFactory extends ModelFactory
+{
+    protected function getDefaults(): array
+    {
+        return [
+            'name' => self::faker()->realText(30),
+            'slug' => self::faker()->slug,
+            'question' => self::faker()->paragraphs(
+                self::faker()->numberBetween(1, 4),
+                true
+            ),
+
+            'askedAt' => rand(1, 10) > 2 ? self::faker()->dateTimeBetween('-100 days', '-1 minute') : null,
+            'votes' => rand(-20, 50),
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        // see https://github.com/zenstruck/foundry#initialization
+        return $this
+            // ->beforeInstantiate(function(Question $question) {})
+            ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Question::class;
+    }
+}
